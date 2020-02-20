561539bf8e249adc1557f6db382eb6f2b5849e4b
elian.nicaise <elian.nicaise@epitech.eu>
2020-02-20 21:33:43 +0100

    adding big file reading

--- src/parser.go
+++ src/parser.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"log"
-	"os"
 	"strings"
 	"strconv"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 )
-
-func parser(path string) header {
+/*
+func parser(path string) (header, library) {
 	fptr := flag.String("fpath", path, "file path to read from")
 	flag.Parse()
 
@@ -25,7 +24,10 @@ func parser(path string) header {
 	}()
 	s := bufio.NewScanner(f)
 	var i int = 0
+	var lib int = -1
+	var tmp int = 0
 	h := header{}
+	l := library{}
 
 	for s.Scan() {
 		str := strings.Split(s.Text(), " ")
@@ -43,21 +45,101 @@ func parser(path string) header {
 				}
 				if err != nil {
 				}
-			} else {
-			}
-			if i == 1 {
+			} else if i == 1 {
 				tmp, err := strconv.Atoi(value)
 				if err != nil {
 					fmt.Println("ERROR\n")
 				}
 				h.scores = append(h.scores, tmp)
+			} else {
+				if i % 2 == 0 {
+					fmt.Println(str)
+					tmp = 1
+				} else {
+					fmt.Println(str)
+				}
 			}
 		}
+		if tmp == 1 {
+			lib++
+		}
+		tmp = 0
 		i++
+		fmt.Println(lib)
 	}
 	err = s.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
-	return h
+	return h, l
+}
+*/
+func parser(filename string) (header, library) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+	}
+	buf := bytes.NewBuffer(file)
+	var i int = 0
+	var lib int = -1
+	var tmp int = 0
+	h := header{}
+	l := library{}
+
+	for {
+		line, err := buf.ReadString('\n')
+		if len(line) == 0 {
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return h, l
+			}
+		}
+		if err != nil && err != io.EOF {
+			return h, l
+		}
+		str := strings.Split(line, " ")
+		for index, value := range str {
+			index = index
+			if i == 0 {
+				if index == 0 {
+					h.books, err = strconv.Atoi(value)
+				}
+				if index == 1 {
+					h.libraries, err = strconv.Atoi(value)
+				}
+				if index == 2 {
+					h.days_scanning, err = strconv.Atoi(value)
+				}
+				if err != nil {
+				}
+			} else if i == 1 {
+				tmp, err := strconv.Atoi(value)
+				if err != nil {
+//					fmt.Println("ERROR\n")
+				}
+				h.scores = append(h.scores, tmp)
+			} else {
+				l = l
+				if i % 2 == 0 {
+//					fmt.Println(str)
+					tmp = 1
+				} else {
+//					fmt.Println(str)
+				}
+			}
+		}
+		if tmp == 1 {
+			lib++
+		}
+		tmp = 0
+		i++
+	}
+	return h, l
+}
+
+func main() {
+	// a flat file that has 339276 lines of text in it for a size of 62.1 MB
+	filename := "../test.txt"
+	fmt.Println(parser(filename))
 }
