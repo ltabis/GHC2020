de97a57630bed70e2ace0cac15660788845001f8
alang <arthur.lang@epitech.eu>
2020-02-20 21:15:50 +0100

    correct type name

--- src/library.go
+++ src/library.go
@@ -1,11 +1,10 @@
-
 package main
 
 type library struct {
-	name		string
-	books    	int
-	signup 	 	int
-	shipping 	int
+	name        int
+	books       int
+	signup      int
+	shipping    int
 	book_scores []int
-	unlocked 	bool
+	unlocked    bool
 }
